5408a8dc79da15fd48de8153795942b6a1cd4d7b
Janosch-Gehring <janosch.gehring99@gmail.com>
2025-03-03 14:50:53 +0100

    fix missing lower() checking for test accounts

--- core/scripts/user_repository.py
+++ core/scripts/user_repository.py
@@ -185,7 +185,7 @@ def assign_to_weakest_group(user_id: str, task: str):
         u_id, user_task, qualified, group, progress, _, data = user
         if (user_task != task) or (qualified != 1) or (u_id == user_id):
             continue
-        if "test" in data["prolific_id"]:
+        if "test" in data["prolific_id"].lower():
             continue
         group_counts[group] += 1
 
