d522d826a7643527b4861460e5f94818055625bb
Selina Meyer <selina.meyer@utn.de>
2025-04-08 14:26:10 +0200

    implemented shuffle for presentation page

--- memory_experiment/pages/presentation_page.py
+++ memory_experiment/pages/presentation_page.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import time
+import random
 
 from core.scripts.utils import read_json_from_file, TASK_INFO, skip_to_next_sample, get_amount_of_samples_for_group, handle_next_button
 from core.scripts import user_repository
@@ -8,25 +9,22 @@ from core.scripts import user_repository
 samples = read_json_from_file(TASK_INFO["memory_experiment"]["annotation_filepath"])
 
 placeholder = st.empty()
-if "progress" not in st.session_state:
-    st.session_state.progress = user_repository.get_checkpoint("memory")
-    if not st.session_state.progress:
-        st.session_state.progress = skip_to_next_sample(0, samples, st.session_state.user[3], 1, "memory", qualification_function=None)
-        st.session_state.presented_samples = 0
-st.session_state.page = "memory_experiment_presentation_page_sample" + str(st.session_state.progress)
-num_samples = get_amount_of_samples_for_group("memory", "memory_experiment", st.session_state.user[3])
-print("Number of Samples to present:", num_samples)
-while st.session_state.presented_samples < num_samples -1: 
-    print("Progress: ", st.session_state.progress)
-    index = int(st.session_state.progress)
-    placeholder.write(samples[str(index)]["headline"])
-    time.sleep(2)
-    st.session_state.progress = skip_to_next_sample(index, samples, st.session_state.user[3], 1, "memory", qualification_function=None)
-    st.session_state.presented_samples += 1
-    print("In loop, presented samples: ", st.session_state.presented_samples)
 
-index = int(st.session_state.progress)
-print("Out of Loop, presetned samples: ", st.session_state.presented_samples)
-placeholder.write(samples[str(index)]["headline"])
-time.sleep(2)
+if "shuffled_keys_presentation" not in st.session_state:
+    st.session_state.progress = user_repository.get_checkpoint("memory")
+    shuffled_keys = [key for key, value in samples.items() if value["grouping"] == st.session_state.user[3]]
+    random.shuffle(shuffled_keys)
+    st.session_state.shuffled_keys_presentation = shuffled_keys
+    print("Added shuffled keys:", st.session_state.shuffled_keys_presentation)
+    st.session_state.index = 0
+
+while st.session_state.index < len(st.session_state.shuffled_keys_presentation): 
+    print("Progress: ", st.session_state.index)
+    index = int(st.session_state.progress)
+    key = st.session_state.shuffled_keys_presentation[st.session_state.index]
+    placeholder.write(samples[key]["headline"])
+    time.sleep(2)
+    st.session_state.index += 1
+    print("In loop, presented samples: ", st.session_state.index)
+
 st.switch_page("memory_experiment/pages/distractor_page.py")
