a16a0265f7f1a6c5bb88e3be26465248df3b6498
Selina Meyer <selina.meyer@utn.de>
2025-04-10 14:44:07 +0200

    added captcha to informed consent page

--- memory_experiment/pages/informed_consent_page.py
+++ memory_experiment/pages/informed_consent_page.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from core.scripts import user_repository, database_repository
+from captcha.image import ImageCaptcha
+import random, string
 
+length_captcha = 6
+width = 200
+height = 150
 target_id = st.session_state.user_id
 user = user_repository.get_user(target_id)
 task = st.session_state.task
@@ -25,12 +30,34 @@ if not user:
                     Praesent at bibendum purus, quis auctor nunc. Nunc nisi tortor, varius volutpat efficitur ut, 
                     ullamcorper a mi. Ut felis nunc, blandit sagittis tellus sit amet, sagittis tristique justo. 
                     Suspendisse potenti.
+
+                    Never refresh the page or go back to the previous page, as this will cause the experiment to crash.
+                    <p>By clicking the button below, you agree to participate in this experiment and give your consent to the use of your data for research purposes.
                     
                     <p>Please enter your prolific ID below:""")
 
             prolific_id = st.text_input("Prolific ID:", max_chars=200)
         if prolific_id:
-            user_repository.create_user(target_id, task=task, data={"prolific_id": prolific_id})
-            user = user_repository.get_user(target_id)
-            st.session_state.user = list(user)
-            st.switch_page("memory_experiment/pages/presentation_page.py")
+            if "captcha_control" not in st.session_state:
+                print("adding captcha control to session state")
+                st.session_state.captcha_control = False
+            col1, col2 = st.columns(2)
+            if "captcha" not in st.session_state:
+                print("adding captcha to session state")
+                st.session_state.captcha = "".join(random.choices(string.ascii_letters + string.digits, k=length_captcha))
+                print("the captcha is: ", st.session_state.captcha)
+            Image = ImageCaptcha(width=width, height=height)
+            data = Image.generate(st.session_state.captcha)
+            col1.image(data)
+            captcha_text = col2.text_input("Please enter the captcha text:", max_chars=length_captcha)
+            if captcha_text:
+                print("Captcha text entered: ", captcha_text)
+                if captcha_text == st.session_state.captcha:
+                    st.session_state.captcha_control = True
+                    st.success("Captcha is correct!")
+                    user_repository.create_user(target_id, task=task, data={"prolific_id": prolific_id})
+                    user = user_repository.get_user(target_id)
+                    st.session_state.user = list(user)
+                    st.switch_page("memory_experiment/pages/presentation_page.py")
+                else:
+                    st.error("Captcha is incorrect. Please try again.")
