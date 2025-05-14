e83f36015036a8815fb7d0bbab5ccf741ceaf161
Janosch-Gehring <janosch.gehring99@gmail.com>
2025-05-14 13:14:15 +0200

    change indexing of smallerfied annotation samples

--- big_eval_ending_task/resources/smallerfy_samples.py
+++ big_eval_ending_task/resources/smallerfy_samples.py
@@ -11,7 +11,8 @@ def main():
     for i in samples:
         sample = samples[i]
         if ("grouping" not in sample) or (sample["grouping"] < NUMBER_OF_WANTED_GROUPS):
-            new_samples[i] = sample
+            sample["og_index"] = i
+            new_samples[len(new_samples)+1] = sample
 
     with open("big_eval_ending_task/resources/annotation_samples_30.json", "w") as f:
         json.dump(new_samples, f, indent=4)
