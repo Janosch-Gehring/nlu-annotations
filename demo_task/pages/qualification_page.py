4a64be8749aa0a4905697130f275ed194efe16d9
Janosch-Gehring <janosch.gehring99@gmail.com>
2025-10-24 14:06:17 +0200

    fix scaling better

--- demo_task/pages/qualification_page.py
+++ demo_task/pages/qualification_page.py
@@ -116,11 +116,11 @@ else:
     gpt_agreeing_percentage = int((precounts[question["gpt_choice"]-1] / max(1, sum(counts)) * 100))
 
     oneshot_user = [0, 0, 0, 0, 0, 0]
-    oneshot_user[user_choice-1] = 1 * sum(counts) * 0.04  # scale it so it remains visible
+    oneshot_user[user_choice-1] = sum(counts) * 0.08  # scale it so it remains visible
     #values = pd.DataFrame(list(zip(precounts, oneshot_user)), columns=["Menschen", "Du"])
 
     oneshot_gpt = [0, 0, 0, 0, 0, 0]
-    oneshot_gpt[question["gpt_choice"] - 1] = 1 * max(1, (sum(counts) * 0.04))
+    oneshot_gpt[question["gpt_choice"] - 1] = sum(counts) * 0.08
 
     st.markdown(f"""
 ### Deine Wahl: {user_choice}
