e29bfb543c13cbf58877b8484620000ff0c569e5
Janosch-Gehring <janosch.gehring99@gmail.com>
2025-11-28 13:34:01 +0100

    remove post qualification option for exercise

--- core/scripts/utils.py
+++ core/scripts/utils.py
@@ -68,8 +68,7 @@ TASK_INFO = {
     "big_eval_ending_task": {
         "annotation_filepath": "big_eval_ending_task/resources/annotation_samples.json",
         "qualification_filepath": "big_eval_ending_task/resources/qualification_questions.json",
-        "number_of_annotator_groups": 130,
-        "group_assignment": "post-qualification"
+        "number_of_annotator_groups": 130
     },
     "big_eval_sentence_task": {
         "annotation_filepath": "big_eval_sentence_task/resources/annotation_samples.json",
